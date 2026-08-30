e777c4a7eb24979d94c626607f83b027af8d005f
smatrchen <smatrchen@qq.com>
2026-08-30 21:09:52 +0800

    fixxxxx

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,19 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// 强制所有 Android 模块(含第三方插件)使用 compileSdk 36,
+// 以满足 flutter_plugin_android_lifecycle 等插件对 API 36 的要求,
+// 否则会因 AAR metadata 检查失败导致构建报错。
+subprojects {
+    afterEvaluate {
+        if (project.plugins.hasPlugin("com.android.library") || project.plugins.hasPlugin("com.android.application")) {
+            project.extensions.configure<com.android.build.gradle.BaseExtension>("android") {
+                compileSdkVersion(36)
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
