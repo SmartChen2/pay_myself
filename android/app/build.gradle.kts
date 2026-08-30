1fc75e638b3d4eb326a65a883b931069aae38c48
smatrchen <smatrchen@qq.com>
2026-08-30 20:46:22 +0800

    bugfix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.payme.paymyself"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
